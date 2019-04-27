8ba0d640bc677c4ed1ebdcceb85225938c222219
git <charles.okey.ifeadi@gmail.com>
2019-04-27 15:53:26 +0200

    1

--- PythonLearning.py
+++ PythonLearning.py
@@ -1,1 +1,26 @@
 
+
+import logging
+
+"""testing git234"""
+
+
+class Generic:
+
+    test = 0
+    def __init__(self, val):
+        self.test = val
+
+        return None
+
+
+
+
+
+def main():
+    v = Generic(6)
+    print("Printing:" + str(v.test))
+
+
+
+main()
